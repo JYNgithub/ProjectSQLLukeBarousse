26ffa7387f528bcb2652e42b02873a2ce3265fcb
Chong <chongjinjye@gmail.com>
2024-11-01 20:49:55 +0800

    progress

--- sql_queries/query3.sql
+++ sql_queries/query3.sql
@@ -1,44 +1,22 @@
--- What are the skills required for the top-paying Data Engineer roles?
--- Objective: Guides candidates to build the skills demanded for the top-paying roles
--- 1) Use the top 10 highest pay jobs data from the first query
--- 2) Identify the specific skills required for those jobs
+-- What are the top skill types based on salary for Data Engineers?
+-- Objective: Identify the most financially rewarding skill types to acquire or improve
+-- 1) Identify the average salary associated with each skill type
+-- 2) Identify the top 10 most financially rewarding skill types
 
-WITH top_10_pay AS (
-    SELECT 
-        jp.job_id,
-        jp.job_title,
-        jp.salary_hour_avg,
-        cd.name company_name
-    FROM 
-        job_postings_fact jp
-    LEFT JOIN
-        company_dim cd
-    ON 
-        jp.company_id = cd.company_id
-    WHERE
-        jp.job_title = 'Data Engineer'
-        AND jp.salary_hour_avg IS NOT NULL
-    ORDER BY
-        jp.salary_hour_avg DESC
-    LIMIT 10
-)
-
-SELECT 
-    sd.skills,
-    count(tp.job_id) demand_for_skill
-FROM 
-    top_10_pay tp
+SELECT
+    sd.type,
+    ROUND(AVG(jp.salary_hour_avg), 2) average_hourly_salary
+FROM    
+    job_postings_fact jp
 INNER JOIN
-    skills_job_dim sj ON tp.job_id = sj.job_id
+    skills_job_dim sj ON jp.job_id = sj.job_id
 INNER JOIN
     skills_dim sd ON sj.skill_id = sd.skill_id
+WHERE 
+    jp.job_title = 'Data Engineer'
+    AND jp.salary_hour_avg IS NOT NULL
 GROUP BY
-    sd.skills
+    sd.type
 ORDER BY
-    demand_for_skill DESC
-LIMIT 20;
-
-
-
-
-
+    average_hourly_salary DESC
+LIMIT 10;
--- sql_queries/query4.sql
+++ sql_queries/query4.sql
@@ -1,23 +1,54 @@
---  What are the most in-demand skills for Data Engineers?
--- Objective: Identify the top 10 skills needed to get a job as a Data Engineer, regardless of salary 
--- 1) Focus on all job postings for Data Engineers
--- 2) Identify the top 10 most demanded skills 
+-- What are the most high-demand and high-paying skills in the field of Data Engineering?
+-- Objective: Identify skills that guarantee job security and favorable compensation
+-- Identify the most high-demand and high-paying skills for all roles related to 'Data Engineer'
+
+WITH skills_demand AS (
+    SELECT
+        sd.skill_id,
+        sd.skills,
+        COUNT(jp.job_id) demand_for_skill
+    FROM    
+        job_postings_fact jp
+    INNER JOIN
+        skills_job_dim sj ON jp.job_id = sj.job_id
+    INNER JOIN
+        skills_dim sd ON sj.skill_id = sd.skill_id
+    WHERE 
+        jp.job_title LIKE '%Data Engineer%'
+        AND jp.salary_hour_avg IS NOT NULL
+    GROUP BY
+        sd.skill_id, sd.skills
+),
+skills_pay AS (
+    SELECT
+        sd.skill_id,
+        sd.skills,
+        ROUND(AVG(jp.salary_hour_avg), 2) average_hourly_salary
+    FROM    
+        job_postings_fact jp
+    INNER JOIN
+        skills_job_dim sj ON jp.job_id = sj.job_id
+    INNER JOIN
+        skills_dim sd ON sj.skill_id = sd.skill_id
+    WHERE 
+        jp.job_title LIKE '%Data Engineer%'
+        AND jp.salary_hour_avg IS NOT NULL
+    GROUP BY
+        sd.skill_id, sd.skills
+)
 
 SELECT
-    sd.skills,
-    COUNT(jp.job_id) demand_for_skill
-FROM    
-    job_postings_fact jp
+    skills_demand.skill_id,
+    skills_demand.skills,
+    skills_demand.demand_for_skill,
+    skills_pay.average_hourly_salary
+FROM
+    skills_demand
 INNER JOIN
-    skills_job_dim sj ON jp.job_id = sj.job_id
-INNER JOIN
-    skills_dim sd ON sj.skill_id = sd.skill_id
-WHERE 
-    jp.job_title = 'Data Engineer'
-GROUP BY
-    sd.skills
+    skills_pay ON skills_demand.skill_id = skills_pay.skill_id
 ORDER BY
-    demand_for_skill DESC
+    skills_demand.demand_for_skill DESC,
+    skills_pay.average_hourly_salary DESC
 LIMIT 10;
 
 
